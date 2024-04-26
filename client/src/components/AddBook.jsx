77ce986515271980f403eafe90f3e05c09c3726d
ramu2245 <126410935+ramu2245@users.noreply.github.com>
2024-04-26 15:58:08 +0530

    Add files via upload

---
+++ client/src/components/AddBook.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import  {useNavigate} from 'react-router-dom'
+
+const AddBook = () => {
+    const [name, setName] = useState('')
+    const [author, setAuthor] = useState('')
+    const [price, setPrice] = useState('')
+    const [imageUrl, setImageUrl] = useState('')
+    const navigate = useNavigate()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.post('http://localhost:3001/book/add', {name, author,price, imageUrl})
+        .then(res => { 
+            if(res.data.added) {
+                navigate('/books')
+            }
+            else {
+                console.log(res)
+            }
+        })
+        .catch(err => console.log(err))
+      }
+
+  return (
+    <div className="student-form-container">
+      <form method='POST' className="student-form" onSubmit={handleSubmit}>
+        <h2>Add Book</h2>
+        <div className="form-group">
+          <label htmlFor="book">Book Name:</label>
+          <input type="text" id="book" name="book" 
+          onChange={(e) => setName(e.target.value)}/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="author">Author Name:</label>
+          <input type="text" id="author" name="author" 
+          onChange={(e) => setAuthor(e.target.value)}/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="price">Price:</label>
+          <input type="text" id="price" name="price" 
+          onChange={(e) => setPrice(e.target.value)}/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="image">Image URL:</label>
+          <input type="text" id="image" name="image" 
+          onChange={(e) => setImageUrl(e.target.value)}/>
+        </div>
+        <button type="submit">Add </button>
+      </form>
+    </div>
+  )
+}
+
+export default AddBook
